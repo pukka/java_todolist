7a0b7ade676550f74c80b613ce337151aea3e90e
FukazawaShun <breakdown547@gmail.com>
2015-04-11 21:45:30 +0900

    Add user create function

--- app/controllers/Users.java
+++ app/controllers/Users.java
@@ -10,9 +10,33 @@ import views.html.*;
 import models.*;
 
 public class Users extends Controller {
+    /** ユーザ作成用のフォーム */
+    static Form<User> userForm = Form.form(User.class);
 
     /** ログインページ */
     public static Result login(){
+	List<User> user = null;
+	user = User.find.all();
+	if(user.isEmpty()){
+	    return redirect( routes.Users.first() );
+	}
         return TODO;
+    }
+
+    /** ユーザが誰もいなければ呼び出される */
+    public static Result first() {
+        return ok( first.render(userForm) );
+    }
+
+    /** ユーザ追加処理 */
+    public static Result addUser() {
+        Form<User> filledForm = userForm.bindFromRequest();
+      	if(filledForm.hasErrors()){
+            return badRequest(first.render(filledForm));
+   	}
+        User.create(filledForm.get());
+        session().clear();
+	session("name",filledForm.get().name);
+	return redirect(routes.Application.showTasks());
     }
 }
