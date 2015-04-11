75169dfc8dfac923e6e7da4af514dcd8fea4bc3f
FukazawaShun <breakdown547@gmail.com>
2015-04-11 20:30:14 +0900

    Add new branch

---
+++ app/models/User.java
@@ -0,0 +1,35 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+import play.db.ebean.*;
+import play.data.format.*;
+import play.data.validation.*;
+
+@Entity 
+public class User extends Model {
+    @Id
+    @Constraints.Min(10)
+    public Long id;
+
+    @Constraints.Required
+    public String name;
+
+    public String password;
+
+    public boolean admin;
+
+    public static Finder<Long, User> find = new Finder<Long,User>(
+        Long.class, User.class
+    );
+
+    /** 新規ユーザ登録 */
+    public static void create(User user){
+        user.save();
+    }
+
+    /** ユーザ認証 */
+    public static User authenticate(String name, String password){
+        return find.where().eq("name", name).eq("password", password).findUnique();
+    }
+}
